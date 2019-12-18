7ee8fb2b7af4083a6089ef5583b50d816fc6a32a
Piotr <pio.skro@gmail.com>
2019-12-18 11:44:52 +0100

    a_ticket_has_many_notes
    a_ticket_has_many_notes

--- app/Ticket.php
+++ app/Ticket.php
@@ -4,7 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Ticket
+ * @package App
+ */
 class Ticket extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    public function notes()
+    {
+        return $this->morphMany(Note::class, 'referable');
+    }
+
 }
--- tests/Unit/TicketTest.php
+++ tests/Unit/TicketTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Unit;
 
+use App\Note;
 use App\Ticket;
 use Tests\TestCase;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 /**
@@ -32,5 +34,15 @@ class TicketTest extends TestCase
         $this->assertNotNull($this->ticket->title);
         $this->assertNotNull($this->ticket->content);
         $this->assertDatabaseHas('users', [ 'id' => $this->ticket->requested_by ]);
+    }
+
+    /** @test */
+    public function a_ticket_has_many_notes()
+    {
+        $this->assertInstanceOf(Collection::class, $this->ticket->notes);
+        $this->assertCount(0, $this->ticket->notes);
+
+        create(Note::class, [ 'referable_type' => Ticket::class, 'referable_id' => $this->ticket->id ]);
+        $this->assertCount(1, $this->ticket->fresh()->notes);
     }
 }
