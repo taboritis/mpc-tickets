18a99a0617bf73b2a56f9519d8de707547d6fdbf
Piotr <pio.skro@gmail.com>
2019-12-19 08:33:08 +0100

    a_typical_user_cannot_create_note_related_to_user

--- tests/Feature/NotesCRUD/NotesCreateTest.php
+++ tests/Feature/NotesCRUD/NotesCreateTest.php
@@ -26,11 +26,23 @@ class NotesCreateTest extends TestCase
      */
     private $user;
 
+    /**
+     * @var string
+     */
+    private $userEndpoint;
+
+    /**
+     * @var string
+     */
+    private $ticketEndpoint;
+
     protected function setUp(): void
     {
         parent::setUp();
         $this->ticket = create(Ticket::class);
         $this->user = create(User::class);
+        $this->userEndpoint = "/api{$this->user->path()}/notes";
+        $this->ticketEndpoint = "/api{$this->ticket->path()}/notes";
     }
 
     /** @test */
@@ -38,9 +50,59 @@ class NotesCreateTest extends TestCase
     {
         $note = [ 'content' => $this->faker->paragraph ];
 
-        $this->json('POST', "/api{$this->user->path()}/notes", $note, $this->headers)
+        $this->json('POST', $this->userEndpoint, $note, $this->headers)
             ->assertUnauthorized();
-        $this->json('POST', "/api{$this->ticket->path()}/notes", $note, $this->headers)
+        $this->json('POST', $this->ticketEndpoint, $note, $this->headers)
             ->assertUnauthorized();
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_create_note_related_to_ticket()
+    {
+        $this->signIn();
+
+        $note = [ 'content' => $this->faker->paragraph ];
+
+        $this->json('POST', $this->ticketEndpoint, $note, $this->headers)
+            ->assertStatus(201);
+
+        $this->assertDatabaseHas('notes', [
+            'referable_type' => Ticket::class,
+            'referable_id' => $this->ticket->id,
+            'content' => $note['content'],
+        ]);
+    }
+
+    /** @test */
+    public function a_support_member_can_create_note_related_to_user()
+    {
+        $this->signInAsSupportMember();
+        $note = [ 'content' => $this->faker->paragraph ];
+
+        $this->json('POST', $this->userEndpoint, $note, $this->headers)
+            ->assertStatus(201);
+
+        $this->assertDatabaseHas('notes', [
+            'referable_type' => User::class,
+            'referable_id' => $this->user->id,
+            'content' => $note['content'],
+        ]);
+    }
+
+    /** @test */
+    public function a_typical_user_cannot_create_note_related_to_user()
+    {
+        $this->signIn();
+
+        $note = [ 'content' => $this->faker->paragraph ];
+
+        $this->json('POST', $this->userEndpoint, $note, $this->headers)
+            ->assertStatus(403);
+
+        $this->assertDatabaseMissing('notes', [
+            'referable_type' => User::class,
+            'referable_id' => $this->user->id,
+            'content' => $note['content'],
+        ]);
     }
 }
